a93dbfee197e62cfca43ac03be032bd774a0e36d
Alireza <alibytesk@gmail.com>
2025-05-08 15:30:29 +0330

    add Profileview with get & put

--- accounts/views.py
+++ accounts/views.py
@@ -10,8 +10,16 @@ from .models import *
 
 
 class ProfileView(APIView):
-
+    serializer_class = UserSerializer
     def get(self, request):
         user = request.user
         serializer = UserSerializer(instance=user, many=False)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        user = request.user
+        serializer = UserSerializer(data=request.data, partial=True, instance=user, many=False)
+        if serializer.is_valid():
+            serializer.update(validated_data=serializer.validated_data, instance=user)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
